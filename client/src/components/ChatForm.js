c322c20aedb9d5a4c9fc2ac76c6899fc2afac3eb
Doğan Can Selek <canselek2434@gmail.com>
2022-01-19 18:39:36 +0300

    update ChatForm

--- client/src/components/ChatForm.js
+++ client/src/components/ChatForm.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useFormik } from 'formik'
+import styles from './styles.module.css'
 
 function ChatForm() {
+
+    const { values, handleSubmit, handleChange, resetForm } = useFormik({
+        initialValues: {
+            message: "",
+        },
+        onSubmit: (values) => {
+            console.log(values.message)
+            resetForm(values.message = "")
+        },
+    })
     return (
         <div>
-            ChatForm
+            <form onSubmit={handleSubmit}>
+                <input className={styles.textInput} name="message" value={values.message} onChange={handleChange} />
+            </form>
         </div>
     )
 }
